a21ae8c9259c5b6c7a64db589a28ed6d51a35168
Kemal Geylani YUKİ <96909207+kemalgeylaniyuki@users.noreply.github.com>
2023-08-08 12:25:46 +0300

    Fragments & Graph & ViewBinding

--- app/src/main/java/com/kemalgeylani/foodbook/FoodDetailFragment.kt
+++ app/src/main/java/com/kemalgeylani/foodbook/FoodDetailFragment.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.kemalgeylani.foodbook.databinding.FragmentFoodDetailBinding
 
 class FoodDetailFragment : Fragment() {
+
+    private var _binding : FragmentFoodDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private var foodId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +25,25 @@ class FoodDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_detail, container, false)
+        //return inflater.inflate(R.layout.fragment_food_detail, container, false)
+        _binding = FragmentFoodDetailBinding.inflate(layoutInflater,container,false)
+        val view = binding.root
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            foodId = FoodDetailFragmentArgs.fromBundle(it).foodId
+            println(foodId)
+        }
+
+        }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
--- app/src/main/java/com/kemalgeylani/foodbook/FoodListFragment.kt
+++ app/src/main/java/com/kemalgeylani/foodbook/FoodListFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.kemalgeylani.foodbook.databinding.FragmentFoodListBinding
 
 
 class FoodListFragment : Fragment() {
+
+    private var _binding : FragmentFoodListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +24,23 @@ class FoodListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_list, container, false)
+        //return inflater.inflate(R.layout.fragment_food_list, container, false)
+        _binding = FragmentFoodListBinding.inflate(layoutInflater,container,false)
+        val view = binding.root
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.goDetailsButton.setOnClickListener {
+            val action = FoodListFragmentDirections.actionFoodListFragmentToFoodDetailFragment(3)
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
